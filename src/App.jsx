d9cd172b8e2fc121a8bc86de49a053c362ad9828
jorgecarmenaescobar <nonzgamer06@gmail.com>
2025-10-27 17:18:38 +0100

    Arreglado import de CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import './styles.css';
+import './index.css';
 
 export default function App() {
   return (
